26413606e4965204838676d79857f49d97206dad
stephaniedefranco <stephanie.defranco@surveysampling.com>
2017-11-10 14:49:47 -0500

    Add controller and object classes

---
+++ AccountController.java
@@ -0,0 +1,71 @@
+package bankservice;
+
+import bankservice.BankAccount;
+import java.util.Scanner;
+
+public class AccountController {
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount(2452.78);
+        bankServices(bankAccount);
+    }
+
+    private static double amountHandler(String service) {
+        double amount;
+        String errMsg = "Invalid amount. Please use positive decimal amounts and do not exceed two decimal points." +
+                "\nPlease enter an amount to ";
+        System.out.println("Please enter an amount to " + service + ": ");
+
+        if (scan.hasNextDouble()) {
+            amount = scan.nextDouble();
+        } else {
+            scan.next();
+            System.out.println(errMsg + service + ": ");
+            amount = scan.nextDouble();
+        }
+        while (amount < 0 || !hasTwoDecimalPoints(amount)) {
+            System.out.println(errMsg + service + ": ");
+            amount = scan.nextDouble();
+        }
+        return amount;
+    }
+
+    private static void bankServices(BankAccount account) {
+
+        Scanner scan = new Scanner(System.in);
+        String command = "";
+
+        while (!command.equals("exit")) {
+            System.out.println("Please enter in a command (Deposit, Withdraw, Balance, Exit) : ");
+            command = (scan.next()).toLowerCase();
+
+            switch (command) {
+                case "deposit":
+                    account.deposit(amountHandler("deposit"));
+                    break;
+                case "withdraw":
+                    account.withdraw(amountHandler("withdraw"));
+                    break;
+                case "balance":
+                    System.out.println("The current balance is $" + account.getBalance());
+                    break;
+                case "exit":
+                    System.exit(0);
+                    break;
+                default:
+                    bankServices(account);
+            }
+        }
+    }
+
+    private static boolean hasTwoDecimalPoints(double amount) {
+        String text = Double.toString(Math.abs(amount));
+        int integerPlaces = text.indexOf('.');
+        int decimalPlaces = text.length() - integerPlaces - 1;
+        return decimalPlaces < 3;
+    }
+
+
+}
---
+++ BankAccount.java
@@ -0,0 +1,25 @@
+package bankservice;
+public class BankAccount {
+
+    private double balance;
+
+    public BankAccount() {
+        balance = 0;
+    }
+
+    public BankAccount(double initialBalance) {
+        balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+}
